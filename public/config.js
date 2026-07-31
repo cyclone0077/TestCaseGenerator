53313ba1073de27b4cbffebad45f601ababed581
mohit.kumar <mohit.kumar@jungleegames.com>
2026-07-31 13:42:53 +0530

    Remove export keyword from config.js - plain script, not ES6 module

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
- export const CONFIG = {
+const CONFIG = {
   BASE_URL: "http://localhost:7860",
   FLOW_ID: "c7aaf8c9-f775-4d50-a7fc-63b2b0892cd5",
   API_KEY: "sk-6rultxRt1Nn-ho1pyIYXm-VSrpmnBzrAhZlmbRN-_Qc",
