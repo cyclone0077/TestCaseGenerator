0df3a3a6f2570e61cded47905a3fe4a98bf682b9
mohit.kumar <mohit.kumar@jungleegames.com>
2026-07-31 12:35:30 +0530

    Add Langflow config

---
+++ public/config.js
@@ -0,0 +1,10 @@
+ export const CONFIG = {
+  BASE_URL: "http://localhost:7860",
+  FLOW_ID: "c7aaf8c9-f775-4d50-a7fc-63b2b0892cd5",
+  API_KEY: "sk-6rultxRt1Nn-ho1pyIYXm-VSrpmnBzrAhZlmbRN-_Qc",
+  MODEL_TWEAKS: {
+    "GoogleGenerativeAIModel": { "model_name": "gemini-3-flash-preview" }
+  },
+  FILE_COMPONENT_ID: "",
+  PREFETCHED_STORY_COMPONENT_ID: "CustomComponent-vKmbB"  // JiraFetchOrPassthrough component
+};
